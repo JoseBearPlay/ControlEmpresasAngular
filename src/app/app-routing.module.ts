0f07583e08d5616b8766c31bc9b07bfcbb99f7a8
Jsalazar <jsalazar-2019070@kinal.edu.gt>
2021-09-25 08:53:54 -0600

    Modificacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpresaComponent } from './componentes/empresa/empresa.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { LoginAdminComponent } from './componentes/login-admin/login-admin.component';
+import { LoginEmpresaComponent } from './componentes/login-empresa/login-empresa.component';
+import { RestriccionService } from './servicios/restriccion.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'loginAdmin', component: LoginAdminComponent},
+  { path: 'loginEmpresa', component: LoginEmpresaComponent},
+  { path: 'inicio', component: InicioComponent, canActivate:[RestriccionService]},
+  { path: 'empresa', component: EmpresaComponent, canActivate:[RestriccionService]},
+  { path: 'empleados', component: EmpresaComponent, canActivate:[RestriccionService]},
+  { path: '**', component: LoginAdminComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
