f49b28bfb800e49d1bff4eeb7f1e3bb13b3dcff7
Jsalazar <jsalazar-2019070@kinal.edu.gt>
2021-09-26 04:56:13 -0600

    Creacion Modelo Producto

---
+++ src/app/modelos/producto.model.ts
@@ -0,0 +1,10 @@
+export class Producto{
+  constructor(
+    public _id: String,
+    public nombre: String,
+    public proveedor: String,
+    public stock: String,
+    public vendido: String,
+    public productoEmpresa: String,
+  ){}
+}
