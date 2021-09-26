6607b27f687efb4acf95b85fe20217e6e1a0d335
Jsalazar <jsalazar-2019070@kinal.edu.gt>
2021-09-26 14:14:18 -0600

    Cambios Pipe Buscar Departamento

--- src/app/pipes/buscar-departamento.pipe.ts
+++ src/app/pipes/buscar-departamento.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class BuscarDepartamentoPipe implements PipeTransform {
 
-  transform(usuarios: any, search: any): any {
+  transform(empleados: any, search: any): any {
 
     if(search == undefined){
-      return usuarios;
+      return empleados;
     } else{
-      return usuarios.filter( usuario=>{
-        return usuario.departamento.toLowerCase().includes(search.toLowerCase())
+      return empleados.filter( empleado=>{
+        return empleado.departamento.toLowerCase().includes(search.toLowerCase());
       })
     }
   }
